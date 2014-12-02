38bee9f8d1e1d5b776fb2f592136c624591c1e35
Charlie Antoun <MadariKanoha@gmail.com>
2014-12-02 11:28:41 -0800

    Added a new title screen

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -1,4 +1,4 @@
-game.TitleScreen = me.ScreenObject.extend({
+game.TitleScreen = me.ScreenObject.extend({ 
     /**	
      *  action to perform on state change
      */
@@ -7,15 +7,14 @@ game.TitleScreen = me.ScreenObject.extend({
         me.game.world.addChild(titleImage, 1);
         me.input.bindKey(me.input.KEY.ENTER, "start");
         
-        me.game.word.addChild(new (me.Renderable.extend ({
+        me.game.world.addChild(new (me.Renderable.extend ({
             init: function(){
                 this._super(me.Renderable, 'init',[510, 30, me.game.viewport.width, me.game.viewport.height]);
-                this.font = new me.Font("Arial", 46, "red")
+                this.font = new me.Font("Arial", 46, "red");
             },
             
             draw: function(renderer) {
-                    this.font.draw(renderer.getContext(), "Mario(?)", 450, 130);
-                    this.font.draw(renderer.getContext(), "Press ENTER to play!", 250, 530);
+                  
             }
             
         })));    
